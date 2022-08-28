d2c05a5848e850b65497d1b1ba80170c62683fca
keerthivel <keerthivel.rajendran@gmail.com>
2022-08-27 22:53:13 -0500

    Initial commit

---
+++ src/main/java/com/keerthivel/shiftmaster/ShiftmasterApplication.java
@@ -0,0 +1,13 @@
+package com.keerthivel.shiftmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftmasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keerthivel/shiftmaster/ShiftmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keerthivel.shiftmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
